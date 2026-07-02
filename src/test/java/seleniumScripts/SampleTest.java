e44bd1fb32f72cc75c075c6caacf45a3ec07b57c
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2026-07-02 09:51:49 +0530

    Sample Test Modify

--- src/test/java/seleniumScripts/SampleTest.java
+++ src/test/java/seleniumScripts/SampleTest.java
@@ -4,7 +4,9 @@ public class SampleTest
 {
 	public static void main(String[] args) {
 		
+		System.out.println("Starting the test");
 		System.out.println("Hello World");
+		System.out.println("Executing the test");
 	}
 
 }
