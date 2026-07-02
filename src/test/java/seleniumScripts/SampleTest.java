f83ce412068dabc4a80211246d45656e76baaa5e
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2026-07-02 09:47:27 +0530

    First Selenium Script

---
+++ src/test/java/seleniumScripts/SampleTest.java
@@ -0,0 +1,10 @@
+package seleniumScripts;
+
+public class SampleTest 
+{
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/seleniumScripts/SwagValidLogin.java
@@ -0,0 +1,45 @@
+package seleniumScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class SwagValidLogin
+{
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void BrowserLaunch() throws InterruptedException
+	{
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/");
+		Thread.sleep(2000);
+	}
+	
+	@Test
+	public void swag_ValidLogin() throws InterruptedException
+	{
+		WebElement userName=driver.findElement(By.id("user-name"));
+		userName.sendKeys("standard_user");
+		Thread.sleep(2000);
+		WebElement password=driver.findElement(By.id("password"));
+		password.sendKeys("secret_sauce");
+		Thread.sleep(2000);
+		WebElement loginButton=driver.findElement(By.id("login-button"));
+		loginButton.click();
+		Thread.sleep(2000);
+	}
+	
+	@AfterMethod
+	public void BrowserClose()
+	{
+		if(driver!=null)
+		{
+			driver.close();
+		}
+	}
+
+}
