bda26650ba9122c6a165efa1f9830d37f6accdb0
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2026-07-02 09:59:29 +0530

    Invalid Login New Script

---
+++ src/test/java/seleniumScripts/SwagInValidLogin.java
@@ -0,0 +1,45 @@
+package seleniumScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class SwagInValidLogin
+{
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void BrowserLaunch() throws InterruptedException
+	{
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/");
+		Thread.sleep(2000);
+	}
+	
+	@Test
+	public void swag_InValidLogin() throws InterruptedException
+	{
+		WebElement userName=driver.findElement(By.id("user-name"));
+		userName.sendKeys("dfhdfhgfhfgh");
+		Thread.sleep(2000);
+		WebElement password=driver.findElement(By.id("password"));
+		password.sendKeys("ghfghfgg");
+		Thread.sleep(2000);
+		WebElement loginButton=driver.findElement(By.id("login-button"));
+		loginButton.click();
+		Thread.sleep(2000);
+	}
+	
+	@AfterMethod
+	public void BrowserClose()
+	{
+		if(driver!=null)
+		{
+			driver.close();
+		}
+	}
+
+}
